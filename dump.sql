451f48df0c368ca7f407949ae8ee2b677596b42f
allanheil1 <allanheil1@gmail.com>
2023-03-01 17:01:18 -0300

    updated: dump.sql

--- dump.sql
+++ dump.sql
@@ -16,6 +16,24 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: links; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.links (
+    id integer NOT NULL,
+    url text NOT NULL,
+    "shortUrl" text NOT NULL,
+    "userId" integer NOT NULL,
+    "visitCount" integer DEFAULT 0 NOT NULL,
+    "createdAt" date DEFAULT now()
+);
+
+
 --
 -- Name: links_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
@@ -29,24 +47,6 @@ CREATE SEQUENCE public.links_id_seq
     CACHE 1;
 
 
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: links; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.links (
-    id integer DEFAULT nextval('public.links_id_seq'::regclass) NOT NULL,
-    url text NOT NULL,
-    "shortUrl" text NOT NULL,
-    "userId" integer NOT NULL,
-    "visitCount" integer DEFAULT 0 NOT NULL,
-    "createdAt" date DEFAULT now()
-);
-
-
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
@@ -58,33 +58,6 @@ CREATE TABLE public.users (
     password text NOT NULL,
     "createdAt" date DEFAULT now()
 );
-
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.users_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
-
---
--- Name: users id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
 
 
 --
@@ -104,45 +77,6 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 --
 
 SELECT pg_catalog.setval('public.links_id_seq', 1, false);
-
-
---
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT pg_catalog.setval('public.users_id_seq', 1, false);
-
-
---
--- Name: links links_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.links
-    ADD CONSTRAINT links_pkey PRIMARY KEY (id);
-
-
---
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- Name: links links_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.links
-    ADD CONSTRAINT "links_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id);
 
 
 --
